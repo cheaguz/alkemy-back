9a2c04e9af55e423798f82e6a00c01eb32fb077f
Cheaguz <aguzf83@gmail.com>
2021-10-20 04:12:17 -0300

    add functions and fix

--- routes/operationRouter.js
+++ routes/operationRouter.js
@@ -8,4 +8,5 @@ router.post('/',operationController.create);
 router.get('/',operationController.getAll)
 router.get('/ingreso' , operationController.getIngreso) //entry
 router.get('/egreso',operationController.getEgreso)     //egress
+router.delete('/' , operationController.delete);
 module.exports = router;
